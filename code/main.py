6527dddea41bc7cff65a438f6842e2611b046609
hank940115 <146960977+hank940115@users.noreply.github.com>
2024-03-20 11:50:37 +0800

    開圖測試
    開圖測試完成，順便自動設定路徑，無論用哪種方式開啟，都能把系統路徑設為這個專案的根目錄

--- code/main.py
+++ code/main.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import os
+os.chdir(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 class HDR:
     '''主要操作的物件'''
     #self.imgs : 圖片物件的陣列
@@ -14,3 +16,10 @@ class HDR:
         img = cv2.imdecode(np.fromfile(filename, dtype=np.uint8), -1)
         self.imgs.append(img)
         self.ltimes.append(ltime)
+
+if __name__ == "__main__":
+    obj = HDR()
+    obj.openImage(r"./data/PPT範例亮圖.png", 0)
+    obj.openImage(r"./data/PPT範例暗圖.png", -1)
+    cv2.imshow(str(obj.ltimes[0]), obj.imgs[0])
+    cv2.waitKey(0)
